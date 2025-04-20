9e7c2dd18302a6714784ec3e101ed15113d573c3
vcxzsa2 <kienv905@gmail.com>
2025-04-20 22:15:38 +0700

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,35 @@
+package bai3;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+class Solution1 {
+
+    // Function to find the data of kth node from
+    // the end of a linked list.
+    int getKthFromLast(Node head, int k) {
+        // Your code here
+        Node fast = head;
+        Node slow = head;
+
+
+        for (int i = 0; i < k; i++) {
+            if (fast == null){
+                return -1;
+            }
+            fast = fast.next;
+        }
+
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow != null ? slow.data : -1;
+    }
+}
